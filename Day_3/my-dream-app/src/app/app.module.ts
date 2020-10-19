6751c7d8918a5716f2819b70bc49096836eb6cb7
Jason <jason.onggo@tempatkerja.com>
2020-10-19 23:00:03 +0700

    move the providers to root

--- Day_3/my-dream-app/src/app/app.module.ts
+++ Day_3/my-dream-app/src/app/app.module.ts
@@ -1,10 +1,18 @@
-import { BrowserModule, HammerModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
+import { Injectable, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthService } from './services/auth.service';
+
+import * as Hammer from 'hammerjs';
+import { BrowserModule, HammerGestureConfig, HammerModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+
+@Injectable()
+export class MyHammerConfig extends HammerGestureConfig {
+  overrides = <any>{
+    swipe: { direction: Hammer.DIRECTION_ALL }
+  }
+}
 
 @NgModule({
   declarations: [
@@ -13,10 +21,12 @@ import { AuthService } from './services/auth.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule,
     HammerModule // only happens to @angular/core ^9. see: https://levelup.gitconnected.com/making-hammerjs-work-with-angular-9-81d289159320
   ],
-  providers: [AuthService],
+  providers: [AuthService, {
+    provide: HAMMER_GESTURE_CONFIG,
+    useClass: MyHammerConfig
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Day_3/my-dream-app/src/app/features/hammerjs/hammerjs.module.ts
+++ Day_3/my-dream-app/src/app/features/hammerjs/hammerjs.module.ts
@@ -1,28 +1,12 @@
-import { Injectable, NgModule } from "@angular/core";
-
-import * as Hammer from 'hammerjs';
-import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
 
 import { HammerJSRoutingModule } from './hammerjs-routing.module';
 import { HammerJSComponent } from './hammerjs.component';
-
-@Injectable()
-export class MyHammerConfig extends HammerGestureConfig {
-  overrides = <any>{
-    swipe: { direction: Hammer.DIRECTION_ALL }
-  }
-}
 
 @NgModule({
   imports: [
     HammerJSRoutingModule,
   ],
   declarations: [HammerJSComponent],
-  providers: [
-    {
-      provide: HAMMER_GESTURE_CONFIG,
-      useClass: MyHammerConfig
-    }
-  ]
 })
 export class HammerJSModule { }
